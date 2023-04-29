87255d90a9b3c840f4c590492da18c59921cb5e4
Hussein Haydar <82262862+hussseinhaiadr6@users.noreply.github.com>
2023-04-29 18:40:55 +0300

    added the remove padding

--- PCKS7.cpp
+++ PCKS7.cpp
@@ -11,6 +11,11 @@ vector<uint8_t> pkcs7_pad(const vector<uint8_t>& data, int block_size) {
     padded_data.resize(data.size() + padding_size, padding_size);
    return padded_data;
 }
+vector<uint8_t> RemovePadding(const vector<uint8_t>& padded_data) {
+    uint8_t padding_size = padded_data.back(); // Get padding size from last byte
+    vector<uint8_t> data(padded_data.begin(), padded_data.end() - padding_size);
+    return data;
+}
 
 int main() {
     vector<uint8_t> data = {'H', 'H', 'l', 'l', 'o','p','e','r','t','y','u','j','H','H','H','H','H'};
@@ -21,7 +26,17 @@ int main() {
     for (uint8_t c : padded_data_hex) {
         cout<<setfill('0')<<hex<<setw(2)<<static_cast<unsigned int>(c) << " ";
     }
-
+    cout<<endl;
+vector<uint8_t>unpadded= RemovePadding(padded_data_hex);
+for (uint8_t c : unpadded) {
+    //in hex format
+        cout<<setfill('0')<<hex<<setw(2)<<static_cast<unsigned int>(c) << " ";
+    }
+    cout<<endl;
+for (uint8_t c : unpadded) {
+      // in string format
+     cout<<static_cast<unsigned char>(c) << " ";
+}
     return 0;
 }
 
